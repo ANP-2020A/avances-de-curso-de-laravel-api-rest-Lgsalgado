05c10644d31e0dbe6f746e331aee5c4efb58e227
lgSalgado <luis.salgado@epn.edu.ec>
2020-07-21 12:40:16 -0500

    #22 Validation Article

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,12 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Article;
 use App\Http\Resources\Article as ArticleResource;
 use App\Http\Resources\ArticleCollection;
+use phpDocumentor\Reflection\Types\Self_;
+
 class ArticleController extends Controller
 {
+    private static $rules=[
+            'title' => 'required|string|unique:articles|max:255',
+            'body' => 'required',
+        ];
+    private static $messages=[
+        'required' => 'El campo :attribute es obligatorio.',
+        'body.required' => 'El body no es válido.'
+    ];
     public function index()
     {
         return new ArticleCollection(Article::paginate());
@@ -19,6 +28,16 @@ class ArticleController extends Controller
     }
     public function store(Request $request)
     {
+        $request->validate(self::$rules, self::$messages);
+//        $messages = [
+//            'required' => 'El campo :attribute es obligatorio.',
+//            'body.required' => 'El body no es válido.'
+//        ];
+//        $validatedData = $request->validate([
+//            'title' => 'required|string|unique:articles|max:255',
+//            'body' => 'required',
+//        ], $messages);
+
         $article = Article::create($request->all());
         return response()->json($article, 201);
     }
