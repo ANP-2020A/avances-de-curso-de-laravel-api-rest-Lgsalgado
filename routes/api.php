a04cdd17b3ebfd3bd943db35a57d34f449e01ccb
lgSalgado <luis.salgado@epn.edu.ec>
2020-07-23 13:26:14 -0500

    #23 Creación y manejo de imágenes

--- routes/api.php
+++ routes/api.php
@@ -21,11 +21,13 @@ Route::post('register', 'UserController@register');
 Route::post('login', 'UserController@authenticate');
 Route::get('articles', 'ArticleController@index');
 
+
 Route::group(['middleware' => ['jwt.verify']], function() {
     Route::get('user', 'UserController@getAuthenticatedUser');
     //Articles
 
     Route::get('articles/{article}', 'ArticleController@show');
+    Route::get('articles/{article}/image', 'ArticleController@image');
     Route::post('articles', 'ArticleController@store');
     Route::put('articles/{article}', 'ArticleController@update');
     Route::delete('articles/{article}', 'ArticleController@delete');
