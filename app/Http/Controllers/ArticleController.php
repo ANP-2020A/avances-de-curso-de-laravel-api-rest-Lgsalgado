f655f98b96a66dbe8832946dc441bdf339e62563
lgSalgado <luis.salgado@epn.edu.ec>
2020-08-27 11:11:22 -0500

    #24 Relaciones polimórficas

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -26,14 +26,18 @@ class ArticleController extends Controller
     }
     public function index()
     {
+
+        //$this->authorize('viewAny', Article::class);
         return new ArticleCollection(Article::paginate());
     }
     public function show(Article $article)
     {
+        $this->authorize('view', $article);
         return response()->json(new ArticleResource($article),200);
     }
     public function store(Request $request)
     {
+        $this->authorize('create', Article::class);
         $request->validate([
             'title' => 'required|string|unique:articles|max:255',
             'body' => 'required',
@@ -50,6 +54,7 @@ class ArticleController extends Controller
     }
     public function update(Request $request, Article $article)
     {
+        $this->authorize('update',$article);
         $request->validate([
             'title' => 'required|string|unique:articles,title,'.$article->id.'|max:255',
             'body' => 'required',
@@ -61,6 +66,7 @@ class ArticleController extends Controller
     }
     public function delete(Request $request, Article $article)
     {
+        $this->authorize('delete', $article);
         $article->delete();
         return response()->json(null, 204);
     }
